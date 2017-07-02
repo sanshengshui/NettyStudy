c8dda9d8002fb9b3b1e51a64dd488491f736a39c
sanshenghui <lovewsic@gmail.com>
2017-07-02 19:34:21 +0800

    WebSocket

---
+++ src/main/java/com/sanshengshui/Netty_In_Action/chapter12/ChatServer.java
@@ -0,0 +1,72 @@
+package com.sanshengshui.Netty_In_Action.chapter12;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.group.ChannelGroup;
+import io.netty.channel.group.DefaultChannelGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.util.concurrent.ImmediateEventExecutor;
+
+import java.net.InetSocketAddress;
+
+/**
+ * @ClassName ChatServer
+ * @author 穆书伟
+ * @description 引导服务器
+ * @date 2017/7/2 19:24
+ */
+public class ChatServer {
+    //创建DefaultChannelGroup,其将保存所有已经连接的WebSocket Channel
+    private final ChannelGroup channelGroup =
+            new DefaultChannelGroup(ImmediateEventExecutor.INSTANCE);
+    private final EventLoopGroup group = new NioEventLoopGroup();
+    private Channel channel;
+    public ChannelFuture start(InetSocketAddress address){
+        //引导服务器
+        ServerBootstrap bootstrap = new ServerBootstrap();
+        bootstrap.group(group)
+                .channel(NioServerSocketChannel.class)
+                .childHandler(createInitializer(channelGroup));
+        ChannelFuture future = bootstrap.bind(address);
+        future.syncUninterruptibly();
+        channel = future.channel();
+        return future;
+    }
+    //创建ChatServerInitializer
+    protected ChannelInitializer<Channel> createInitializer(
+            ChannelGroup group
+    ){
+        return new ChatServerInitializer(group);
+    }
+
+    //处理服务器关闭，并释放所有资源
+    public void destroy(){
+                if(channel!=null){
+                    channel.close();
+                }
+                channelGroup.close();
+                group.shutdownGracefully();
+    }
+
+    public static void main(String[] args){
+        if(args.length!=1){
+            System.err.println("Please give port as argument");
+            System.exit(1);
+        }
+        int port = Integer.parseInt(args[0]);
+        final ChatServer endpoint = new ChatServer();
+        ChannelFuture future = endpoint.start(new InetSocketAddress(port));
+        Runtime.getRuntime().addShutdownHook(new Thread(){
+            @Override
+            public void run() {
+                endpoint.destroy();
+            }
+        });
+        future.channel().closeFuture().syncUninterruptibly();
+    }
+
+}
